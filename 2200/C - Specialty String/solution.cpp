1df28997865e2b10e302bb65c4c3a959907c2a27
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:40:31 +0530

    Add Specialty String [C] from Codeforces

---
+++ 2200/C - Specialty String/solution.cpp
@@ -0,0 +1,76 @@
+    /**
+     *    author:  Sanjeev Kumar Gupta
+     *    created: 28.02.2026 21:48:20
+    **/
+ 
+    #include <bits/stdc++.h>
+ 
+    // --- For Debugging ---
+    #ifdef LOCAL
+    #include "algo/debug.h"
+    #else
+    #define debug(...) 42
+    #endif
+    // -------------------
+ 
+    using namespace std;
+ 
+    #define ll long long int
+    #define endl '
+'
+ 
+    // --- Type Definitions ---
+    typedef vector<ll> vi;
+    typedef vector<vi> vvi;
+    typedef pair<ll, ll> ii;
+    typedef vector<ii> vp;
+ 
+    // --- Shorthand Macros ---
+    #define all(v) v.begin(), v.end()
+    #define srt(v) sort(all(v))
+    #define pb push_back
+ 
+    // --- I/O Helper Functions ---
+    void vin(vi &a, ll n) {
+        a.resize(n);
+        for (ll i = 0; i < n; i++) cin >> a[i];
+    }
+ 
+    void pin(const vi &a) {
+        for (size_t i = 0; i < a.size(); i++) {
+            cout << a[i] << (i == a.size() - 1 ? "" : " ");
+        }
+        cout << endl;
+    }
+ 
+    // --- Main Solve Function ---
+    void solve() {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        stack<char> st;
+        for(char c:s){
+            if(!st.empty() && st.top()==c){
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        if(st.empty())cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;    
+    }
+ 
+    // --- Main Function ---
+    int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+ 
+        int t = 1;
+        cin >> t;
+        while (t--) {
+            solve();
+        }
+ 
+        return 0;
+    }
