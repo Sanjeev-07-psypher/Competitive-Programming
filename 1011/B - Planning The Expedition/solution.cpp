bb406a5b5437615655f931baa23d75fbb8e0875a
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:32:13 +0530

    Add Planning The Expedition [B] from Codeforces

---
+++ 1011/B - Planning The Expedition/solution.cpp
@@ -0,0 +1,96 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 19.06.2026 22:44:47
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+bool check(vector<ll> &p,ll m,ll mid){
+    ll count=0;
+    
+    for(int i=0;i<p.size();i++){
+        count+=(p[i]/mid);
+    }
+    return count>=m;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll m,n;
+    cin>>m>>n;
+    vector<ll> v(n);
+    for(auto &it :v) cin>>it;
+    map<ll,ll> mp;
+    for(auto x:v) mp[x]++;
+    vector<ll> p;
+    for(auto x: mp){
+        p.push_back(x.second);
+    }
+    sort(p.rbegin(),p.rend());
+    ll l=1;
+    ll h=n;
+    ll ans=0;
+    while(l<=h){
+        ll mid=l+(h-l)/2;
+        if(check(p,m,mid)){
+            ans=mid;
+            l=mid+1;
+        } else {
+            h=mid-1;
+        }
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
