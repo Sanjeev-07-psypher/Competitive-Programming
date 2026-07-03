35019406c285826811e02581c5476d5d17c4e4d2
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:29:13 +0530

    Add Matryoshkas [D] from Codeforces

---
+++ 1790/D - Matryoshkas/solution.cpp
@@ -0,0 +1,78 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 01.06.2026 22:11:55
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    sort(v.begin(),v.end());
+    map<ll,ll> mp;
+    for(auto &x:v){
+        mp[x]++;
+    }
+    ll ans=mp.begin()->second;
+ 
+    for(auto it=next(mp.begin()); it!=mp.end(); it++){
+        ll x = it->first;
+        ans += max(0LL,mp[x]-mp[x-1]);
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
