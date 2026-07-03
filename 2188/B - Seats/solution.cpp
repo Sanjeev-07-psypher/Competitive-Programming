a19c0ba9b52f3395155b9368a4f9135f45278ebc
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:22:34 +0530

    Add Seats [B] from Codeforces

---
+++ 2188/B - Seats/solution.cpp
@@ -0,0 +1,82 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 11.03.2026 08:27:15
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    s='1'+s+'1';
+    int ans=0,l=0;
+    for(int i=1;i<=n;i++){
+        if (s[i]=='0'){
+            if(s[i-1]=='1'){
+                l=i;
+            }
+            if(s[i+1]=='1'){
+                int c=(l == 1) +(i == n);
+                ans+=(i-l+1+c)/3;
+            }
+        }else{
+            ++ans;
+        }
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function -
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
