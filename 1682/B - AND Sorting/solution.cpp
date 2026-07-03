3c511bf8ce60d430c2c0e4c70eba6b32685b12d7
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:19:10 +0530

    Add AND Sorting [B] from Codeforces

---
+++ 1682/B - AND Sorting/solution.cpp
@@ -0,0 +1,76 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 20.03.2026 19:28:26
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function --
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    ll ans = (1<<30)-1;
+    for(int i=0;i<n;i++){
+        if(v[i]!=i){
+            ans=ans&v[i];
+        }
+    }
+    cout<<ans<<endl;
+    
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
