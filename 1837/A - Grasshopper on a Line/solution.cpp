1c6a365a942a4dffb34985be398ce4bd5fa560c2
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:32:24 +0530

    Add Grasshopper on a Line [A] from Codeforces

---
+++ 1837/A - Grasshopper on a Line/solution.cpp
@@ -0,0 +1,69 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 25.12.2025 14:35:28
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int x,k;
+    cin>>x>>k;
+    if(x%k!=0){
+        cout<<1<<endl<<x<<endl;
+    }else{
+        cout<<2<<endl<<x-1<<" "<<1<<endl;
+    }
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
