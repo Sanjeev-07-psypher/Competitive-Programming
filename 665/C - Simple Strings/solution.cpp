6e0c12bd7f16751d36840329c82786befd6c074d
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:34:20 +0530

    Add Simple Strings [C] from Codeforces

---
+++ 665/C - Simple Strings/solution.cpp
@@ -0,0 +1,78 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 19.06.2026 22:20:53
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    string s;
+    cin>>s;
+    ll n=s.size();
+    vector<ll> freq(26,0);
+    for(int i=1;i<n;i++){
+        if(s[i]==s[i-1]){
+            for(char c='a'; c<='z';c++){
+                if(c!=s[i-1] && (i+1== n|| c!=s[i+1])){
+                    s[i]=c;
+                    break;
+                }
+            }
+        }
+    }
+    cout<<s<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
