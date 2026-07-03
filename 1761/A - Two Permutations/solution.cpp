fc3ddab69d7e3c1149957380aa6b82b15c759a1d
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:34:36 +0530

    Add Two Permutations [A] from Codeforces

---
+++ 1761/A - Two Permutations/solution.cpp
@@ -0,0 +1,69 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 26.12.2025 13:58:47
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define nl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n,a,b;
+    cin>>n>>a>>b;
+    if((a+b+2)<=n || (a==n && b==n)){
+        cout<<"Yes"<<nl;
+    } else {
+        cout<<"No"<<nl;
+    }
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
