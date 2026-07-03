fc755f03a587ab5e4c5a046388f2570ac6e742cb
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:25:50 +0530

    Add Stable Groups [C] from Codeforces

---
+++ 1539/C - Stable Groups/solution.cpp
@@ -0,0 +1,85 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 03.06.2026 12:51:39
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n,k,x;
+    cin>>n>>k>>x;
+    vector<ll> v(n);
+    for(auto &it :v ) cin>>it;
+    srt(v);
+ 
+    vector<ll> diff;
+    for(int i=0;i<n-1;i++){
+        if(v[i+1]-v[i]>x){
+            diff.push_back(v[i+1]-v[i]);
+        }
+    }
+    srt(diff);
+    ll ans = diff.size()+1; //no of groups initially
+    for(auto val: diff){
+        ll curr = val/x  + (val%x!=0) -1;
+        if(k>=curr){
+            ans--;
+            k-=curr;
+        }
+    }
+    cout<<ans<<endl;
+    
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
