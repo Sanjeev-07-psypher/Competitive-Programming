70cd87ec0d99e145a19a8ee62ed8cae960f6a449
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:35:45 +0530

    Add Subset Multiplication [C] from Codeforces

---
+++ 2124/C - Subset Multiplication/solution.cpp
@@ -0,0 +1,100 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 29.06.2026 14:33:07
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ll gcd(ll a, ll b){
+    a=llabs(a);
+    b=llabs(b);
+    while(b!=0){
+        ll r = a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b){
+   if(a==0 || b==0) return 0;
+   return (a / gcd(a,b)) * b;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n),aux;
+    for(auto &it:v) cin>>it;
+    ll g=v[0];
+    for(int i=0;i<n-1;i++){
+        aux.push_back(v[i]/gcd(v[i],v[i+1]));
+    }
+    // pin(aux);
+    ll ans=1;
+    for(int i=0;i<aux.size();i++){
+        ans=lcm(ans,aux[i]);
+    }
+    // if(aux.size()>0){
+    //     ll g=aux[0];
+    //     for(int i=0;i<aux.size();i++){
+    //         g=gcd(g,aux[i]);
+    //     }
+    //     cout<<g<<endl;
+    // }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
