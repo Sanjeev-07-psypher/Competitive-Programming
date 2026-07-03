f69da5b85a9a5cfcff07a4036909e069e62ed92b
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:37:43 +0530

    Add Make It Zero [A] from Codeforces

---
+++ 1869/A - Make It Zero/solution.cpp
@@ -0,0 +1,78 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 03.02.2026 19:24:49
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(auto &it:v) cin>>it;
+ 
+    if(n%2==0){
+        cout<<2<<endl;
+        cout<<1<<" "<<n<<endl;
+        cout<<1<<" "<<n<<endl;
+    } else {
+        cout<<4<<endl;
+        cout<<1<<" "<<n-1<<endl;
+        cout<<1<<" "<<n-1<<endl;
+        cout<<n-1<<" "<<n<<endl;
+        cout<<n-1<<" "<<n<<endl;
+    }
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
