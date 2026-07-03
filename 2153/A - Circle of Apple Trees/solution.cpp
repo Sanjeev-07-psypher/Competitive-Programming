423557b2d163c740705359ec3a19eadfe1042e3a
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:41:20 +0530

    Add Circle of Apple Trees [A] from Codeforces

---
+++ 2153/A - Circle of Apple Trees/solution.cpp
@@ -0,0 +1,71 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 11.03.2026 08:27:15
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(auto &it :v) cin>>it;
+    set<int> s;
+    for(int i=0;i<n;i++){
+        s.insert(v[i]);
+    }
+    cout<<s.size()<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
