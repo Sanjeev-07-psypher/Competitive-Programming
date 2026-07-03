60f5992eb91d586f89e79a75d7faca549a821942
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:31:24 +0530

    Add Sequence Game [B] from Codeforces

---
+++ 1862/B - Sequence Game/solution.cpp
@@ -0,0 +1,83 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 23.12.2025 17:45:35
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int> brr;
+    brr.reserve(2*n);   // capacity only, size = 0
+ 
+    brr.push_back(arr[0]);
+    for(int i=1;i<n;i++){
+        if(arr[i-1]>arr[i]){
+            brr.push_back(arr[i]);
+        }
+        brr.push_back(arr[i]);
+    }
+    cout<<brr.size()<<endl;
+    for(int i=0;i<brr.size();i++){
+        cout<<brr[i]<<" ";
+    }
+    cout<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
