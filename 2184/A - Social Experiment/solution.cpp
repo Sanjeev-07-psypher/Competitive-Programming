2ab05424be489a756454db9ffccb205088587d1a
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:36:02 +0530

    Add Social Experiment [A] from Codeforces

---
+++ 2184/A - Social Experiment/solution.cpp
@@ -0,0 +1,75 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 25.01.2026 12:39:53
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    if(n<=3){
+        cout<<n<<endl;
+        return;
+    }
+    else if(n%2==0){
+        cout<<0<<endl;
+        return;
+    } else {
+        cout<<1<<endl;
+        return;
+    }
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
