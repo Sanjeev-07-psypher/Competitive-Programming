8deb058f001538695b113ecd051b8f7eb0a34b65
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:30:38 +0530

    Add Line Trip [A] from Codeforces

---
+++ 1901/A - Line Trip/solution.cpp
@@ -0,0 +1,80 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 22.12.2025 14:50:25
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n,x;
+    cin>>n>>x;
+    n+=1;
+    int arr[n];
+    arr[0]=0;
+    for(int i=1;i<n;i++){
+        cin>>arr[i];
+    }
+    int dist[n];
+    for(int i=0;i<n-1;i++){
+        dist[i]=arr[i+1]-arr[i];
+    } 
+    dist[n-1]=2*(x-arr[n-1]);
+    int max=-1;
+    for(int i=0;i<n;i++){
+        if(max<dist[i]) max=dist[i];
+    }
+    cout<<max<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
