64890c76525aca3056e7a744571c896ab2f8ff5f
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:23:40 +0530

    Add Blocked [A] from Codeforces

---
+++ 2220/A - Blocked/solution.cpp
@@ -0,0 +1,79 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 16.04.2026 16:46:18
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    sort(v.rbegin(),v.rend());
+    if(n>1){
+        for(int i=1;i<n;i++){
+            if(v[i]==v[i-1]){
+                cout<<-1<<endl;
+                return;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
