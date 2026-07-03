e707db6467d6aeac732db336f066d6e0c12d714d
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:28:06 +0530

    Add Palindrome, Twelve and Two Terms [B] from Codeforces

---
+++ 2234/B - Palindrome, Twelve and Two Terms/solution.cpp
@@ -0,0 +1,73 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 07.06.2026 20:34:03
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+ 
+    ll a=n%12;
+    if(n==10){
+        cout<<-1<<endl;
+        return;
+    }
+    if(a==10) a=22;
+    cout<<a<<" "<<n-a<<endl;
+ 
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
