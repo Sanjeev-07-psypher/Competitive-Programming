b75d02c9b05c79fecc19d08e4e3038fde299de5b
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:25:13 +0530

    Add Assembly via Minimums [C] from Codeforces

---
+++ 1857/C - Assembly via Minimums/solution.cpp
@@ -0,0 +1,77 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 01.06.2026 12:26:27
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> a;
+    ll newSize= (n*(n-1))/2;
+    vector<ll> b(newSize);
+    for(auto &it: b) cin>>it;
+    sort(b.begin(),b.end());
+ 
+    ll i=0;
+    for(int x=n-1;x>=1;x--){
+        a.push_back(b[i]);
+        i+=x;
+    }
+    a.push_back(b[newSize-1]);
+    pin(a);
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
