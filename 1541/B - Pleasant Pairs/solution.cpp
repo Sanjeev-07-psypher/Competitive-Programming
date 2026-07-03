92136d2645a06f16388cf9f7ea8b75acd93b57ab
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:25:54 +0530

    Add Pleasant Pairs [B] from Codeforces

---
+++ 1541/B - Pleasant Pairs/solution.cpp
@@ -0,0 +1,79 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 03.06.2026 13:49:56
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    vector<pair<ll,ll>> p;
+    for(int i=0;i<n;i++){
+        p.push_back({v[i],i+1});
+    }
+    sort(p.begin(),p.end());
+    ll ans=0;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(p[i].first*p[j].first>=2*n) break;
+            if(p[i].first*p[j].first==p[i].second+p[j].second) ans++;
+        }
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
