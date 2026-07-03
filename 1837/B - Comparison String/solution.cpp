c2929964787581f4fbc1f8499b81a57cf315f2f3
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:34:51 +0530

    Add Comparison String [B] from Codeforces

---
+++ 1837/B - Comparison String/solution.cpp
@@ -0,0 +1,80 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 21.01.2026 23:40:53
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int l=0,sub=2;
+    if(n==1) cout<<2<<endl;
+    else{
+        for(int i=1;i<s.size();i++){
+            if(s[l]==s[i]){
+                sub = max(sub,i-l+2);
+            } else {
+                l=i;
+            }
+        }
+        cout<<sub<<endl;
+    }
+    
+    
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
