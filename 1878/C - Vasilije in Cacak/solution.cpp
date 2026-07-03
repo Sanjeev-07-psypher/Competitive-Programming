17d6b57768727337e19996973131846297312580
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:36:21 +0530

    Add Vasilije in Cacak [C] from Codeforces

---
+++ 1878/C - Vasilije in Cacak/solution.cpp
@@ -0,0 +1,69 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 27.12.2025 20:50:41
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n,k,x;
+    cin>>n>>k>>x;
+    ll y=n-k;
+    ll maxSum = ((n*(n+1))/2)-((y*(y+1))/2);
+    ll minSum = (k*(k+1))/2;
+    if(x<=maxSum && x>=minSum) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
