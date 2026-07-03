0771396ede4777cd11ac8c92e6cc2b5434b6bfae
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:19:41 +0530

    Add Robin Hood in Town [C] from Codeforces

---
+++ 2014/C - Robin Hood in Town/solution.cpp
@@ -0,0 +1,84 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 23.03.2026 21:28:13
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define lld long double
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    if(n==1 || n==2){
+        cout<<-1<<endl;
+        return;
+    }
+    srt(v);
+    lld sum=accumulate(v.begin(), v. end(), 0LL);
+    lld avg=sum/n;
+    ll mid=n/2;
+    ll ans=0;
+    if(n==1 || n==2){
+        cout<<-1<<endl;
+        return;
+    }
+    if(v[mid]>=avg/2){
+        ans=v[mid]*2*n-sum+1;
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
