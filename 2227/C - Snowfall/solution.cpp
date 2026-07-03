f1a4f05e1dce8f019ff67d9c4bbd5f8853200f60
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:24:14 +0530

    Add Snowfall [C] from Codeforces

---
+++ 2227/C - Snowfall/solution.cpp
@@ -0,0 +1,86 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 01.05.2026 08:38:18
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    vector<ll> six,three,two,aux;
+    for(int i=0;i<n;i++){
+        if(v[i]%6==0) six.push_back(v[i]);
+        else if(v[i]%3==0) three.push_back(v[i]);
+        else if(v[i]%2==0) two.push_back(v[i]);
+        else aux.push_back(v[i]);
+    }
+    for(int i=0;i<six.size();i++){
+        cout<<six[i]<<" ";
+    }
+    for(int i=0;i<two.size();i++){
+        cout<<two[i]<<" ";
+    }
+    for(int i=0;i<aux.size();i++){
+        cout<<aux[i]<<" ";
+    }
+    for(int i=0;i<three.size();i++){
+        cout<<three[i]<<" ";
+    }
+    cout<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
