d51e45a16bc933bd8fcf5b2907ffec4a4dd9cd22
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:28:02 +0530

    Add Euclid, Sequence and Two Numbers [A] from Codeforces

---
+++ 2234/A - Euclid, Sequence and Two Numbers/solution.cpp
@@ -0,0 +1,75 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 07.06.2026 20:19:02
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it :v) cin>>it;
+ 
+    sort(v.rbegin(),v.rend());
+    for(int i=0;i<n-2;i++){
+        if(v[i]%v[i+1]!=v[i+2]){
+            cout<<-1<<endl;
+            return;
+        }
+    }
+    cout<<v[0]<<" "<<v[1]<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
