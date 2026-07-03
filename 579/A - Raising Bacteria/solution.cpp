625de03004ad85c449b0d4ce061071926fbe3d0c
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:37:35 +0530

    Add Raising Bacteria [A] from Codeforces

---
+++ 579/A - Raising Bacteria/solution.cpp
@@ -0,0 +1,65 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 02.02.2026 19:53:43
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int x;
+    cin>>x;
+    cout<<__builtin_popcount(x)<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
