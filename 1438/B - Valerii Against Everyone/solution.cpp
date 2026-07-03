0b959a12f066ea6fc38de299525c6eb351b711f0
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:40:35 +0530

    Add Valerii Against Everyone [B] from Codeforces

---
+++ 1438/B - Valerii Against Everyone/solution.cpp
@@ -0,0 +1,73 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 06.02.2026 19:52:00
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+ 
+    set<int> s;
+    for(auto p:v){
+        s.insert(p);
+    }
+    if(s.size()<n) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
