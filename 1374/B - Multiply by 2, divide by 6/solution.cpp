4abc52ba4fa7c77de28a717b869450ee432d6829
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:35:00 +0530

    Add Multiply by 2, divide by 6 [B] from Codeforces

---
+++ 1374/B - Multiply by 2, divide by 6/solution.cpp
@@ -0,0 +1,85 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 22.01.2026 13:16:11
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+bool powerof2(ll n) {
+    return n > 0 && (n & (n - 1)) == 0;
+}
+ 
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    int ops=0;
+    int flag=false;
+    while(n!=1){
+            if(n%6==0){
+                ops++;
+                n/=6;
+            } else if(n%3==0){
+                n*=2;
+                ops++;
+            } else{
+                cout<<-1<<endl;
+                return;
+            }
+        
+    }
+    cout<<ops<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
