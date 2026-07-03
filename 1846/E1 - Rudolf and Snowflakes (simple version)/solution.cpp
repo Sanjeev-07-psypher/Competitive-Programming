f603d52796f27d569aafe39d594780c6a44436e4
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:32:26 +0530

    Add Rudolf and Snowflakes (simple version) [E1] from Codeforces

---
+++ 1846/E1 - Rudolf and Snowflakes (simple version)/solution.cpp
@@ -0,0 +1,85 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 21.06.2026 17:45:58
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ 
+// --- Main Solve Function ---
+void solve(set<ll> st) {
+    ll n;
+    cin>>n;
+    if(st.count(n)){
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    set<ll> st;
+ 
+for(ll r=2; r<=1000; r++){
+    ll cur = r*r;
+    ll sum = 1 + r + cur;
+ 
+    while(sum <= 1000000){
+        st.insert(sum);
+ 
+        if(cur > 1000000/r) break;
+        cur *= r;
+        sum += cur;
+    }
+}
+    while (t--) {
+        solve(st);
+    }
+ 
+    return 0;
+}
