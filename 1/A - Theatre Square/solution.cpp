082dbeaaae0599a7f724ae34c508a2ac16e3bb8a
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:22:14 +0530

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.cpp
@@ -0,0 +1,67 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 08.04.2026 13:42:14
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n,m,a;
+    cin>>n>>m>>a;
+    ll h= (n+a-1)/a;
+    ll v=(m+a-1)/a;
+    cout<<h*v*1LL<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
