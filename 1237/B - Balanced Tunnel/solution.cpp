56364b919fb0e7b8529f75525411c1ab93861d27
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:38:09 +0530

    Add Balanced Tunnel [B] from Codeforces

---
+++ 1237/B - Balanced Tunnel/solution.cpp
@@ -0,0 +1,99 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 02.07.2026 20:54:26
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ll gcd(ll a, ll b){
+    a=llabs(a);
+    b=llabs(b);
+    while(b!=0){
+        ll r = a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b){
+   if(a==0 || b==0) return 0;
+   return (a / gcd(a,b)) * b;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> a(n),b(n);
+    map<ll,ll> pos;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+        pos[b[i]]=i;
+    }
+    ll mx=-1;
+    ll ans=0;
+    for(auto x:a){
+        if(pos[x]<mx)
+            ans++;
+        else
+            mx=pos[x];
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
