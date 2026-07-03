a313f2592f1a9db23caed893c2a9635b48458fe9
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:32:28 +0530

    Add Unit Array [A] from Codeforces

---
+++ 1834/A - Unit Array/solution.cpp
@@ -0,0 +1,79 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 25.12.2025 14:43:02
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int negCount=0,posCount=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==-1) negCount++;
+        else posCount++;
+    }
+    int count=0;
+    while(posCount<negCount || negCount%2==1){
+        count++;
+        posCount++;
+        negCount--;
+    }
+    cout<<count<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
