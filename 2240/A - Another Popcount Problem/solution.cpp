03f2f0c46f62556b9ada4d11cf6a0068c1beeabc
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:34:39 +0530

    Add Another Popcount Problem [A] from Codeforces

---
+++ 2240/A - Another Popcount Problem/solution.cpp
@@ -0,0 +1,75 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 24.06.2026 20:16:27
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    ll ans=0;
+    ll curr=1;
+    while(n>=curr){
+        ll x=n/curr;
+        ll b=min(k,x);
+        ans+=b;
+        n-=(b*curr);
+        curr*=2;
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
