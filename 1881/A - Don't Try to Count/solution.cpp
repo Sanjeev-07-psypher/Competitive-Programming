1bc5a587db0c82002dcb8c40f48cc8dd9775a808
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:31:05 +0530

    Add Don't Try to Count [A] from Codeforces

---
+++ 1881/A - Don't Try to Count/solution.cpp
@@ -0,0 +1,86 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 22.12.2025 21:34:37
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n,m;
+    cin>>n>>m;
+    string x,s;
+    cin>>x;
+    cin>>s;
+    int count =0,i=0;
+    bool flag = false;
+    while(x.size()<s.size()){
+        x+=x;
+        count++;
+    }
+    while(i<3){
+        if(x.find(s)!=string::npos){
+            cout<<count<<endl;
+            flag=true;
+            break;
+        }
+        else{
+            x+=x;
+            count++;
+        }
+        i++;
+    }
+    if(!flag) cout<<-1<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
