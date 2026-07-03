d4821430c9d96096fd4ec7fca7aab1c67a4d97e7
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:21:15 +0530

    Add Neural Feud [D] from Codeforces

---
+++ 2214/D - Neural Feud/solution.cpp
@@ -0,0 +1,67 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 12.03.2026 20:42:52
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ 
+// --- Main Solve Function ---
+void solve(){
+    vector<string> v={"walk","no","no","no","yes","yes","backwards","seven"};
+    int n;
+    cin>>n;
+    cout<<v[n-1]<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
