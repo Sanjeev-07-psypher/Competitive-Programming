1ffa35877217364b13f078abc5bfe57dd620a539
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:23:32 +0530

    Add Petya and Countryside [B] from Codeforces

---
+++ 66/B - Petya and Countryside/solution.cpp
@@ -0,0 +1,91 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 15.04.2026 19:39:19
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function --
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    ll count=0;
+    ll ans=1;
+    for(int i=0;i<n;i++){
+        count=1;
+        //left check
+        ll l=i;
+        while(l>0){
+            if(v[l]>=v[l-1]) {
+                l--;
+                count++;
+            }
+            else break;
+        }
+        //right check
+        ll r=i;
+        while(r<n-1){
+            if(v[r]>=v[r+1]){
+                 count++;
+                 r++;
+            }
+            else break;
+        }
+        ans=max(ans,count);
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
