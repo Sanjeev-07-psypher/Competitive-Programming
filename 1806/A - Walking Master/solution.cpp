cf1a2782b4341a8a2fab65185a9aee0d861e891d
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:32:46 +0530

    Add Walking Master [A] from Codeforces

---
+++ 1806/A - Walking Master/solution.cpp
@@ -0,0 +1,83 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 25.12.2025 17:10:50
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int operations=0;
+    if(b<=d){
+        while(b<d){
+            operations++;
+            a++;
+            b++;
+        }
+        if(a<c){
+            cout<<-1<<endl;
+        } else {
+            while(a!=c){
+                operations++;
+                a--;
+            }
+            cout<<operations<<endl;
+        }
+    }else{
+        cout<<-1<<endl;
+    }
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
