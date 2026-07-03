9b48336c843803f2a92b3a8c6276420046c592c5
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:32:47 +0530

    Add Insane Problem [E] from Codeforces

---
+++ 2044/E - Insane Problem/solution.cpp
@@ -0,0 +1,73 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 22.06.2026 21:05:49
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve(){
+    ll k,l1,r1,l2,r2,a=0,p=1;
+    cin>>k>>l1>>r1>>l2>>r2;
+    while(1){
+        ll l=max(l1,(l2+p-1)/p),r=min(r1,r2/p);
+        if(l<=r)   
+            a+=r-l+1;
+        if(r2/p<k) break;
+        p*=k;
+    }
+    cout<<a<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
