02c656ccdac5aa805ace37a924989740435e00d2
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:32:43 +0530

    Add Kirei Attacks the Estate [E] from Codeforces

---
+++ 2114/E - Kirei Attacks the Estate/solution.cpp
@@ -0,0 +1,92 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 22.06.2026 21:05:49
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+ll m0[200005],m1[200005],a[200005];
+vector<int>g[200005];
+void d(int u,int p){
+    if(p==0){
+        m0[u]=a[u];
+        m1[u]=-a[u];
+    }
+    else{
+        m0[u]=a[u]+max(0LL,m1[p]);
+        m1[u]=-a[u]+max(0LL,m0[p]);
+    }
+    for(int v:g[u])
+        if(v!=p)d(v,u);
+}
+void solve(){
+    int n;cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        g[i].clear();
+    }
+    for(int i=1;i<n;i++){
+        int u,v;
+        cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    d(1,0);
+    for(int i=1;i<=n;i++)
+        cout<<m0[i]<<" ";
+    cout<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
