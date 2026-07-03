4505f627e2ffbec521772dc2a6872972c5958ef4
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:41:36 +0530

    Add Friendly Numbers [A] from Codeforces

---
+++ 2197/A - Friendly Numbers/solution.cpp
@@ -0,0 +1,78 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 12.03.2026 20:42:52
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ll sum(ll y){
+    ll s=0;
+    while(y>0){
+        s+=y%10;
+        y/=10;
+    }
+    return s;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    ll count=0;
+    for(ll y=n;y<=n+300;y++){
+        if(y-sum(y)==n) count++;
+    }
+    cout<<count<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
