d459df6216527ed6ebbdfc152102815474d44b64
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:41:01 +0530

    Add Your Name [B] from Codeforces

---
+++ 2167/B - Your Name/solution.cpp
@@ -0,0 +1,76 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 10.03.2026 23:19:44
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    string name;
+    cin>>s>>name;
+    sort(s.begin(),s.end());
+    sort(name.begin(),name.end());
+    for(int i=0;i<n;i++){
+      if(s[i]!=name[i]){
+        cout<<"NO"<<endl;
+        return;
+      }
+    }
+    cout<<"YES"<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
