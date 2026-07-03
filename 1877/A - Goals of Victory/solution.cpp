64ba5fb0c76b707c667fe9ad8e76a2374ab0858b
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 19:31:13 +0530

    Add Goals of Victory [A] from Codeforces

---
+++ 1877/A - Goals of Victory/solution.cpp
@@ -0,0 +1,71 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 23.12.2025 13:19:08
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> arr(n-1);
+    int sum=0;
+    for(int i=0;i<n-1;i++){
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    cout<<(sum*-1)<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
