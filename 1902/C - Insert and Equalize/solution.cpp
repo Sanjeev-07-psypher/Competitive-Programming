d75b69b82d3181cf29559b18e59f5db971a977a6
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-03 20:35:28 +0530

    Add Insert and Equalize [C] from Codeforces

---
+++ 1902/C - Insert and Equalize/solution.cpp
@@ -0,0 +1,118 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 28.06.2026 16:55:04
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ll gcd(ll a, ll b){
+    a=llabs(a);
+    b=llabs(b);
+    while(b!=0){
+        ll r = a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n),aux(n),ans;
+    for(auto &it:v) cin>>it;
+    srt(v);
+    if(n==1){
+        cout<<1<<endl;
+        return;
+    }
+    ll mx=v[n-1];
+    for(int i=0;i<n;i++){
+        aux[i]=v[i]-mx;
+    }
+    ll x=aux[0];
+    for(int i=1;i<n;i++){
+        x=gcd(x,aux[i]);
+    }
+    ll ops=0;
+    for(int i=0;i<n;i++){
+        ll o=((mx-v[i])/x);
+        ans.push_back(o);
+        ops+=o;
+    }
+    srt(ans);
+    ll val=0;
+    bool flag=false;
+    ll m=0;
+    for(int i=0;i<ans.size();i++){
+        if(i==ans.size()-1){
+            flag=true;
+        }
+        if(ans[i]!=val){
+            m=val;
+            break;
+        } else {
+            val++;
+        }
+    }
+    if(val>n){
+        ops+= (n-1);
+    } else {
+        ops+= val;
+    }
+    cout<<ops<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
