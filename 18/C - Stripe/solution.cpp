066087f2e9e6409e12e5c0cb0a28c448bbc00187
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-07 22:25:07 +0530

    Add Stripe [C] from Codeforces

---
+++ 18/C - Stripe/solution.cpp
@@ -0,0 +1,103 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 07.07.2026 22:31:21
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ll gcd(ll a, ll b){
+    a=llabs(a);
+    b=llabs(b);
+    while(b!=0){
+        ll r = a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b){
+   if(a==0 || b==0) return 0;
+   return (a / gcd(a,b)) * b;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    vector<ll> pre(n),suf(n);
+    pre[0]=v[0];
+    suf[n-1]=v[n-1];
+    for(int i=1;i<n;i++){
+        pre[i]=pre[i-1]+v[i];
+    }
+    for(int i=n-2;i>=0;i--){
+        suf[i]=suf[i+1]+v[i];
+    }
+    // pin(pre);
+    // pin(suf);
+ 
+    ll ans=0;
+    ll r=1;
+    for(int i=0;i<n-1;i++,r++){
+        if(pre[i]==suf[r]){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
