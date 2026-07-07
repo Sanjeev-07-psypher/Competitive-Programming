dfb91f0e88068764b9f5a5e5f724f11da48df817
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-07 19:41:35 +0530

    Add 2^Sort [G] from Codeforces

---
+++ 1692/G - 2^Sort/solution.cpp
@@ -0,0 +1,98 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 07.07.2026 19:21:23
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ll gcd(ll a, ll b){
+    a=llabs(a);
+    b=llabs(b);
+    while(b!=0){
+        ll r = a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b){
+   if(a==0 || b==0) return 0;
+   return (a / gcd(a,b)) * b;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    ll ans=0;
+    ll l=0;
+    bool flag=false;
+    for(int r=l+1;r<n;r++){
+        if(v[r-1]>=2*v[r]){
+            l=r;
+        }
+ 
+        if(r-l==k){
+            // cout<<v[l]<<" "<<v[r]<<endl;
+            ans++;
+            l++;
+        }
+    }
+    cout<<ans<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
