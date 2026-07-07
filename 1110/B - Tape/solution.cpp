bbbc3950f0b7cefe8fb155535b676b418b12f5f2
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-07 21:35:31 +0530

    Add Tape [B] from Codeforces

---
+++ 1110/B - Tape/solution.cpp
@@ -0,0 +1,94 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 07.07.2026 21:32:46
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+ll gcd(ll a, ll b){
+    a=llabs(a);
+    b=llabs(b);
+    while(b!=0){
+        ll r = a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b){
+   if(a==0 || b==0) return 0;
+   return (a / gcd(a,b)) * b;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n,m,k;
+    cin>>n>>m>>k;
+    vector<ll> v(n);
+    for(auto &it:v) cin>>it;
+    vector<ll> gaps;
+    for(int i=1;i<n;i++){
+        gaps.push_back(v[i]-v[i-1]-1);
+    }
+    srt(gaps);
+    ll ans=n;
+    for(int i=0;i<n-k;i++){
+        ans+=gaps[i];
+    }
+    cout<<ans<<endl;
+ 
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
