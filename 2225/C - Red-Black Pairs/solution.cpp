2ce447787fa0c4ed458d92574d94a1561ed112e8
Sanjeev-07-psypher <sanjeevkrgupta17@gmail.com>
2026-07-08 22:17:50 +0530

    Add Red-Black Pairs [C] from Codeforces

---
+++ 2225/C - Red-Black Pairs/solution.cpp
@@ -0,0 +1,78 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 07.06.2026 20:19:02
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+    string s1,s2;
+    cin>>s1>>s2;
+    vector<ll> dp(n+1,1e9);
+    dp[0]=0;
+    for(int i=0;i<n;i++){
+        ll cv=min((s1[i]!='R')+(s2[i]!='R'),(s1[i]!='B')+(s2[i]!='B'));
+        dp[i+1]=min(dp[i+1],dp[i]+cv);
+        if(i+1<n){
+            ll ct=min((s1[i]!='R')+(s1[i+1]!='R'),(s1[i]!='B')+(s1[i+1]!='B'));
+            ll cb=min((s2[i]!='R')+(s2[i+1]!='R'),(s2[i]!='B')+(s2[i+1]!='B'));
+            dp[i+2]=min(dp[i+2],dp[i]+ct+cb);
+        }
+    }
+    cout<<dp[n]<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
