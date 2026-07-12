57643f1c55d5d0057c88ef6c471923e419fc5542
Sanjeev Kumar Gupta <sanjeevkrgupta17@gmail.com>
2026-07-12 20:23:31 +0530

    Add ezraft and Array [B] from Codeforces

---
+++ 2246/B - ezraft and Array/solution.cpp
@@ -0,0 +1,84 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 07.06.2026 20:34:03
+**/
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define endl '
+'
+const int mod = 1e9+7;
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+ 
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+ 
+// --- Main Solve Function ---
+void solve() {
+    ll n;
+    cin>>n;
+ 
+    if(n==1){
+        cout<<1<<endl;
+        return;
+    }
+    if(n==2){
+        cout<<-1<<endl;
+        return;
+    }
+    vector<ll> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+ 
+    ll x=6;
+    for(int i=3;i<n;i++){
+        v.push_back(x);
+        x*=2;
+    }
+    pin(v);
+}
+ 
+// --- Main Function ---
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
