040cefedd4e33887a1f8b8bda890042705f57c09
Sanjeev Kumar Gupta <sanjeevkrgupta17@gmail.com>
2026-08-02 12:25:14 +0530

    Add Alternating Path [D] from Codeforces

--- 2204/D - Alternating Path/solution.cpp
+++ 2204/D - Alternating Path/solution.cpp
@@ -1,76 +1,54 @@
-/**
- *    author:  Sanjeev Kumar Gupta
- *    created: 13.07.2026
-**/
- 
 #include<bits/stdc++.h>
 using namespace std;
  
-#define ll long long
-#define endl '
-'
- 
-void solve(){
-    ll n,m;
-    cin>>n>>m;
- 
-    vector<vector<ll>> adj(n+1);
- 
-    for(ll i=0;i<m;i++){
-        ll u,v;
-        cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
- 
-    vector<ll> color(n+1,-1);
-    ll ans=0;
- 
-    for(ll i=1;i<=n;i++){
-        if(color[i]!=-1) continue;
- 
-        queue<ll> q;
-        q.push(i);
-        color[i]=0;
- 
-        ll cnt0=1,cnt1=0;
-        bool bip=true;
- 
-        while(!q.empty()){
-            ll u=q.front();
-            q.pop();
- 
-            for(auto v:adj[u]){
-                if(color[v]==-1){
-                    color[v]=color[u]^1;
- 
-                    if(color[v]==0) cnt0++;
-                    else cnt1++;
- 
-                    q.push(v);
-                }
-                else if(color[v]==color[u]){
-                    bip=false;
-                }
-            }
+void dfs(int u,vector<vector<int>>&adj,vector<int>&col,int &a,int &b,bool &ok){
+    if(col[u]==0)a++;
+    else b++;
+    for(int v:adj[u]){
+        if(col[v]==-1){
+            col[v]=col[u]^1;
+            dfs(v,adj,col,a,b,ok);
         }
- 
-        if(bip) ans+=max(cnt0,cnt1);
+        else if(col[v]==col[u]){
+            ok=false;
+        }
     }
- 
-    cout<<ans<<endl;
 }
  
-int32_t main(){
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
  
-    ll t;
+    int t;
     cin>>t;
  
     while(t--){
-        solve();
-    }
+        int n,m;
+        cin>>n>>m;
  
-    return 0;
+        vector<vector<int>>adj(n+1);
+ 
+        for(int i=0;i<m;i++){
+            int u,v;
+            cin>>u>>v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+ 
+        vector<int>col(n+1,-1);
+        int ans=0;
+ 
+        for(int i=1;i<=n;i++){
+            if(col[i]==-1){
+                int a=0,b=0;
+                bool ok=true;
+                col[i]=0;
+                dfs(i,adj,col,a,b,ok);
+                if(ok)ans+=max(a,b);
+            }
+        }
+ 
+        cout<<ans<<"
+";
+    }
 }
