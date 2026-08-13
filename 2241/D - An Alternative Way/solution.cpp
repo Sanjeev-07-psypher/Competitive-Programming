aac780fe2902d83e6b65a0042a010fc62bdb3a1d
Sanjeev Kumar Gupta <sanjeevkrgupta17@gmail.com>
2026-08-13 21:03:11 +0530

    Add An Alternative Way [D] from Codeforces

---
+++ 2241/D - An Alternative Way/solution.cpp
@@ -0,0 +1,100 @@
+/**
+ *    author:  Sanjeev Kumar Gupta
+ *    created: 14.07.2026 20:29:38
+ */
+ 
+#include <bits/stdc++.h>
+ 
+// --- For Debugging ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+// -------------------
+ 
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define endl '
+'
+const int mod = 1e9+7;
+ 
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll,ll> ii;
+typedef vector<ii> vp;
+ 
+// --- Shorthand Macros ---
+#define all(v) v.begin(),v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+ 
+// --- I/O Helper Functions ---
+void vin(vi &a,ll n){
+    a.resize(n);
+    for(ll i=0;i<n;i++) cin>>a[i];
+}
+ 
+void pin(const vi &a){
+    for(size_t i=0;i<a.size();i++){
+        cout<<a[i]<<(i==a.size()-1?"":" ");
+    }
+    cout<<endl;
+}
+ 
+ll gcd(ll a,ll b){
+    a=llabs(a);
+    b=llabs(b);
+    while(b!=0){
+        ll r=a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+ 
+ll lcm(ll a,ll b){
+    if(a==0 || b==0) return 0;
+    return (a/gcd(a,b))*b;
+}
+ 
+// --- Main Solve Function ---
+void solve(){
+    ll n;
+    cin>>n;
+ 
+    vi a,b;
+    vin(a,n);
+    vin(b,n);
+ 
+    ll sum=0;
+ 
+    for(int i=0;i<n;i++){
+        sum+=b[i]-a[i];
+ 
+        if(sum<0){
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+ 
+    cout<<"YES"<<endl;
+}
+ 
+// --- Main Function ---
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t=1;
+    cin>>t;
+ 
+    while(t--){
+        solve();
+    }
+ 
+    return 0;
+}
